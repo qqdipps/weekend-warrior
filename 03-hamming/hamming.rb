65d419d74e2dacc78d5947c5cdb5b7b3681611a0
qqdipps <sdipp007@gmail.com>
2019-02-22 11:31:42 -0800

    added logic to class and fixed method signature, test pass

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,4 +1,11 @@
 class Hamming
-  def compute
+  def self.compute(str1, str2)
+    raise ArgumentError.new if str1.length != str2.length
+    return 0 if str1 == str2
+    ham = 0
+    str1.length.times do |i|
+      ham += 1 if str1[i] != str2[i]
+    end
+    return ham
   end
 end
