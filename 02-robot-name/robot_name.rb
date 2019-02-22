5f0829998c72d23be464b3dca989b80a0ba405be
qqdipps <sdipp007@gmail.com>
2019-02-22 10:57:01 -0800

    added Class Robot

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,16 @@
+class Robot
+  attr_accessor :name
 
+  def initialize
+    @name = auto_gen_name
+  end
+
+  def reset
+    @name = auto_gen_name
+  end
+
+  def auto_gen_name
+    alpha = [*"A".."Z"]
+    @name = alpha.sample(2).join + rand(1000).to_s.rjust(3, "0")
+  end
+end
